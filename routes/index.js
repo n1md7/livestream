b60f86b11a7ffab8d4060c0d5c678c8e2fbc8189
bichiko <b_kodua@cu.edu.ge>
2018-07-27 17:34:04 +0300

    creating rooms and joining

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/:id?/', function(req, res, next) {
+	res.render('index', { 
+		title: 'Express',
+		liveId: req.params.id
+	});
+});
+
+module.exports = router;
